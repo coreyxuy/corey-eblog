7e20b765360ce841b8c0be039625a4423ef16bf2
corey <1595162370@qq.com>
2020-11-10 18:40:50 +0800

    自定义模板添加

---
+++ src/main/java/com/itcorey/config/FreemarkerConfig.java
@@ -0,0 +1,24 @@
+package com.itcorey.config;
+
+import com.itcorey.template.PostsTemplate;
+import com.itcorey.template.TimeAgoMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class FreemarkerConfig {
+
+    @Autowired
+    private freemarker.template.Configuration configuration;
+
+    @Autowired
+    PostsTemplate postsTemplate;
+
+    @PostConstruct
+    public void setUp() {
+        configuration.setSharedVariable("timeAgo", new TimeAgoMethod());
+        configuration.setSharedVariable("posts", postsTemplate);
+    }
+}

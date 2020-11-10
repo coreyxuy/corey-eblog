c12a7a827a90ba1461aabdaf825f2db137004222
corey <1595162370@qq.com>
2020-11-10 10:48:39 +0800

    详情页和详情列表提交

--- src/main/java/com/itcorey/controller/PostController.java
+++ src/main/java/com/itcorey/controller/PostController.java
@@ -1,6 +1,9 @@
 package com.itcorey.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -8,14 +11,23 @@ import com.itcorey.controller.BaseController;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author 公众号：徐买狼
  * @since 2020-11-10
  */
-@RestController
-@RequestMapping("/post")
+@Controller
 public class PostController extends BaseController {
+
+    @GetMapping("/category/{id:\\d*}")
+    public String category(@PathVariable(name = "id") Long id) {
+        return "post/category";
+    }
+
+    @GetMapping("/post/{id:\\d*}")
+    public String detail(@PathVariable(name = "id") Long id) {
+        return "post/detail";
+    }
 
 }
